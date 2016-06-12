8472fdeb28b888184a298a2eb716074a77ef3913
Yiguang Lu <lukmylyg@gmail.com>
2016-06-13 00:12:48 +0800

    remove assignment to ajust zsh configuration
    DO REMEMBER to define `MY_SHELL_PLUGINS` in .bashrc or .zshrc

--- my-shell-scripts.sh
+++ my-shell-scripts.sh
@@ -1,7 +1,7 @@
 MY_SHELL_SCRIPTS=${MY_SHELL_SCRIPTS:-"$HOME/.oh-my-shell-scripts"}
 
-MY_SHELL_DEFAULT_PLUGINS=(emacs python)
-MY_SHELL_PLUGINS=${MY_SHELL_PLUGINS:-$MY_SHELL_DEFAULT_PLUGINS}
+# MY_SHELL_DEFAULT_PLUGINS=(emacs python)
+# MY_SHELL_PLUGINS=${MY_SHELL_PLUGINS:-$MY_SHELL_DEFAULT_PLUGINS}
 
 for plugin in ${MY_SHELL_PLUGINS[@]}; do
     source $MY_SHELL_SCRIPTS/plugins/$plugin/$plugin.plugin.sh
